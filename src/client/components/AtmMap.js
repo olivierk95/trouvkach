10cf231eec6b43a08051023eab8e1e1c7dc197f2
badou <badou@pop-os.localdomain>
2019-02-20 15:23:59 +0100

    add sass

--- src/client/components/AtmMap.js
+++ src/client/components/AtmMap.js
@@ -9,17 +9,17 @@
 import * as React from "react";
 
 import Map from "./map";
-import styles from "./styles/styles";
+import "../scss/app.scss";
 
 export default class AtmMap extends React.Component {
     render() {
         return (
-            <div className={styles.container}>
-                <h1 className={styles.title}>{"Check ATM "}</h1>
+            <div className="container">
+                <h1>{"Check ATM "}</h1>
                 <hr />
                 <br />
-                <div className={styles.map_container}>
-                    <Map className={styles.map} />
+                <div>
+                    <Map />
                 </div>
             </div>
         );
